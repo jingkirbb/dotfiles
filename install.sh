68fdff8b56669822e8e7a64aa554e5958c122970
Dmitrii Abramov <4176812+jingkirbb@users.noreply.github.com>
2018-03-21 12:02:43 -0700

    stuff

--- install.sh
+++ install.sh
@@ -8,11 +8,3 @@ find ~/p/dotfiles/dots -name '.*' | xargs -I % ln -sf % ~
 cat $DOTFILES_PATH/ohmyzsh.sh | sh
 sudo echo `which bash` >> /etc/shells
 chsh -s /bin/zsh
-
-# curl -o $HOME/vim.tar.bz2 ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
-# bzcat $HOME/vim.tar.bz2 > $HOME/vim.tar
-# tar xvf $HOME/vim.tar
-# cd $HOME/vim74 && make
-# ln -sf $HOME/vim74/src/vim $HOME/bin/
-git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-$HOME/bin/vim +PluginInstall +qall
